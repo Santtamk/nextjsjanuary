f671dda6b7e44f43d28a2c66a9f95079b2204bab
Santam Kumai <santtamk@gmail.com>
2025-01-06 16:11:07 +0530

    display card added for startup with demo data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
-import SearchForm from "../components/SearchForm";
+import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
+import { title } from "process";
 
 export default async function Home({
   searchParams,
@@ -6,6 +8,18 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Adrion" },
+      description: "This is a description",
+      image: "https://picsum.photos/200",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
   return (
     <>
       <section className="pink_container">
@@ -18,6 +32,21 @@ export default async function Home({
         </p>
 
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search result for "${query}"` : "All Startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
